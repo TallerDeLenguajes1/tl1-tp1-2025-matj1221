f85cd3fccfb1aa9ed6366bd9c8d2bd5875228023
[matj1221] <matiasjcalderon@hotmail.com>
2025-04-03 21:24:28 -0300

    apartado e del punto 1 completado

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,5 +3,15 @@
 int main() {
     printf("Hola mundo.\n");
 
+    int num = 5;
+    int *dir_num;
+    dir_num = &num;
+
+    printf("Contenido del puntero: %i\n", *dir_num);
+    printf("La direccion de memoria almacenada por el puntero: %p\n", dir_num);
+    printf("La direccion de memoria de la variable: %p\n", &num);
+    printf("Direccion de memoria del puntero: %p\n", &dir_num);
+    printf("Tamanio de memoria de num (variable tipo entera): %i\n", sizeof(num));
+
     return 0;
 }
